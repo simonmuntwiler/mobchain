b03bd0763c2c38c27fc5f5ea7107cfca6fd22810
Simon Muntwiler <simonmu@student.ethz.ch>
2018-02-14 18:16:01 +0100

    account struct

--- src/contracts/mobchain.sol
+++ src/contracts/mobchain.sol
@@ -1,7 +1,22 @@
-pragma solidity ^0.4.16
+pragma solidity ^0.4.16;
 
 contract mobchain {
-    
+struct user {
+    uint256 addressLender;
+    bool busy;
+}
+struct lender {
+    uint256 carPosition;
+    uint256 carCondition;
+    bool available;
+    uint256 addressPreuser;
+    bool carType;    
+}
+struct account {
+    user carUser;
+    lender carLender;
+}   
+
 
 
 }
