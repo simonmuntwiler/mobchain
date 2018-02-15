8de895c26d5e95146f52b882b3c7614264d35813
Simon Muntwiler <simonmu@student.ethz.ch>
2018-02-15 10:26:00 +0100

    expected cost and discount function

--- src/contracts/mobchain.sol
+++ src/contracts/mobchain.sol
@@ -25,9 +25,32 @@ function Mobchain public {
 
 } 
 
+address userAddress;
 mapping(address => account) accounts;
 
-function expectedCost()
+function expectedCost(int256 distance, int256 additionalCost) public returns(int256) {
+    userAddress = msg.sender;
+    int256 distancePrice = 1;
+    int256 cost = distance * distancePrice;
+    int256 discountFactor = getDiscount();
+    int256 expCost = cost * discountFactor / 100000 + additionalCost;
+    int256 mobBalance = 10000; //
+    if (mobBalance < expCost) {
+        return (-1);
+    }
+    else {
+    return (expCost);
+    } 
+}
+
+function getDiscount() public pure returns(int256) {
+    //RepToken.balanceOf(userAddress) * 1000;
+    //SusToken.balanceOf(userAddress);
+    int256 repBalance = 50000;
+    int256 susBalance = 100000;
+    int256 discountFactor = repBalance / 100 * 20 - 10000 +susBalance / 100 * 20 - 10000 + 100000;
+    return (discountFactor);
+}
 
 
 
