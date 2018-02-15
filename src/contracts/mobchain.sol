9a3999658fb216fc906d90e3e3d35a25f2b989dc
Simon Muntwiler <simonmu@student.ethz.ch>
2018-02-15 15:20:54 +0100

    minor fixes

--- src/contracts/mobchain.sol
+++ src/contracts/mobchain.sol
@@ -8,6 +8,7 @@ contract MobChain {
     struct User {
         address addressLender;
         bool busy;
+        int256 expCost;
     }
     struct Lender {
         uint256 carPosition;
@@ -36,7 +37,6 @@ contract MobChain {
     }
 
     function addUser(string _name, bool _carType) payable public {
-
         require(msg.value > 0.25 ether);
         accounts[msg.sender].name = _name;
         accounts[msg.sender].carLender.carType = _carType;
@@ -45,13 +45,11 @@ contract MobChain {
         //give mobcoins to user
         MobCoin mobcoin = MobCoin(mobcoinAddress);
         mobcoin.transfer(msg.sender, msg.value * 1000);
-
     }
 
     function expectedCost(int256 distance, int256 additionalCost) public returns(int256) {
-        userAddress = msg.sender;
         expCost = getCost(int256 distance, int256 additionalCost);
-        accounts[userAddress].carUser.expCost = expCost;
+        accounts[msg.sender].carUser.expCost = expCost;
         MobCoin mobcoin = MobCoin(mobcoinAddress);
         int256 mobBalance = mobcoin.balanceOf[msg.sender];
         
@@ -61,50 +59,48 @@ contract MobChain {
         else {
             return (expCost);
         } 
-
-
-}
-
-function getCost(int256 distance, int256 additionalCost) public returns(int256) {
-    int256 distancePrice = 1;
-    int256 cost = distance * distancePrice;
-    int256 discountFactor = getDiscount();
-    int256 expCost = cost * discountFactor / 100000 + additionalCost;
-    return (expCost);
-}
-
-function getDiscount() public pure returns(int256) {
-    RepToken reptoken = RepToken(reptokenAddress);//RepToken.balanceOf(userAddress) * 1000;
-    SusToken sustoken = SusToken(sustokenAddress);//SusToken.balanceOf(userAddress);
-    int256 repBalance = reptoken.balanceOf[msg.sender];
-    int256 susBalance = sustoken.balanceOf[msg.sender];
-    int256 discountFactor = 120000 -repBalance / 100 * 20 -susBalance / 100 * 20;
-    return (discountFactor);
-}
-
-function rate(uint256 carCondition) public {
-    address lenderAddress = accounts[userAddress].carUser.lenderAddress;
-    address preuserAddress = accounts[lenderAddress].carLender.preuserAddress;
-    require(preuserAddress != 0x0)
-    RepToken reptoken = RepToken(reptokenAddress);
-    if (carCondition < accounts[lenderAddress].carLender.carCondition) {
-        reptoken.update(msg.sender, -5);
     }
-    else {
-        reptoken.update(msg.sender, 1);
 
+    function getCost(int256 distance, int256 additionalCost) public returns(int256) {
+        int256 distancePrice = 1;
+        int256 cost = distance * distancePrice;
+        int256 discountFactor = getDiscount();
+        int256 expCost = cost * discountFactor / 100000 + additionalCost;
+        return (expCost);
     }
+
+    function getDiscount() public pure returns(int256) {
+        RepToken reptoken = RepToken(reptokenAddress);//RepToken.balanceOf(userAddress) * 1000;
+        SusToken sustoken = SusToken(sustokenAddress);//SusToken.balanceOf(userAddress);
+        int256 repBalance = reptoken.balanceOf[msg.sender];
+        int256 susBalance = sustoken.balanceOf[msg.sender];
+        int256 discountFactor = 120000 -repBalance / 100 * 20 -susBalance / 100 * 20;
+        return (discountFactor);
+    }
+
+    function rate(uint256 carCondition) public {
+        address lenderAddress = accounts[msg.sender].carUser.lenderAddress;
+        address preuserAddress = accounts[lenderAddress].carLender.preuserAddress;
+        require(preuserAddress != 0x0)
+        RepToken reptoken = RepToken(reptokenAddress);
+        if (carCondition < accounts[lenderAddress].carLender.carCondition) {
+            reptoken.update(preuserAddress, -5);
+        }
+        else {
+            reptoken.update(preuserAddress, 1);
+        }
 
         accounts[lenderAddress].carLender.preuserAddress = 0x0;
         accounts[lenderAddress].carLender.carCondition = carCondition;
     }
 
     function arrive() public pure returns(int256) {
-        require(accounts[userAddress].carUser.busy == 0);
-        accounts[userAddress].carUser.busy = 1;
-        accounts[accounts[userAddress].carUser.addressLender].carLender.available = 0;
+        require(accounts[msg.sender].carUser.busy == 0);
+        accounts[msg.sender].carUser.busy = 1;
+        address lenderAddress = accounts[msg.sender].carUser.addressLender;
+        accounts[lenderAddress].carLender.available = 0;
 
-        int256 finalCost = accounts[userAddress].carUser.expCost;
+        int256 finalCost = accounts[msg.sender].carUser.expCost;
 
         //transfer final cost from user to lender
         MobCoin mobcoin = MobCoin(mobcoinAddress);
@@ -113,18 +109,17 @@ function rate(uint256 carCondition) public {
 
         //update sustainability
         SusToken sustoken = SusToken(sustokenAddress);
-        if (accounts[accounts[userAddress].carUser.addressLender].carLender.carType == 1) { //1 is electr.car
+        if (accounts[accounts[msg.sender].carUser.addressLender].carLender.carType == 1) { //1 is electr.car
         sustoken.update(msg.sender, 5); 
         } 
         else {
         sustoken.update(msg.sender, -5); 
         }
 
-    accounts[accounts[userAddress].carUser.addressLender].carLender.available = 1;
-    accounts[userAddress].carLender.preuser = userAddress; 
-    accounts[userAddress].carUser.busy = 0;
-    return (finalCost);
-   
-}
+        accounts[lenderAddress].carLender.available = 1;
+        accounts[lenderAddress].carLender.preuser = msg.sender; 
+        accounts[msg.sender].carUser.busy = 0;
+        return (finalCost);
+    }
 
 }
