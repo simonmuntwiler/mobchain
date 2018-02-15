ed5d17e92d7e7f69a427a0902f94fc25b99e0eaf
Yannik Nager <ynager@student.ethz.ch>
2018-02-15 10:45:06 +0100

    working on init of everything

--- src/contracts/mobchain.sol
+++ src/contracts/mobchain.sol
@@ -1,34 +1,51 @@
 pragma solidity ^0.4.16;
 
-import "./mobcoin.sol"
-import "./reptoken.sol"
-import "./sustoken.sol"
+import  "./mobcoin.sol";
+import  "./reptoken.sol";
+import  "./sustoken.sol";
 
-contract Mobchain {
-struct user {
-    uint256 addressLender;
-    bool busy;
-}
-struct lender {
-    uint256 carPosition;
-    uint256 carCondition;
-    bool available;
-    uint256 addressPreuser;
-    bool carType;    
-}
-struct account {
-    user carUser;
-    lender carLender;
-} 
+contract MobChain {
+    struct User {
+        address addressLender;
+        bool busy;
+    }
+    struct Lender {
+        uint256 carPosition;
+        uint8 carCondition;
+        bool available;
+        address addressPreuser;
+        bool carType;    
+    }
+    struct Account {
+        string name;
+        User carUser;
+        Lender carLender;
+    }
 
-function Mobchain public {
+    //Account[] accounts;
+    mapping(address => Account) accounts;
+    address mobcoinAddress;
+    address reptokenAddress;
+    address sustokenAddress;
+    
+    function MobChain() public {
+        mobcoinAddress = new MobCoin();
+        reptokenAddress = new RepToken();
+        sustokenAddress = new SusToken();
 
-} 
+    }
 
-mapping(address => account) accounts;
+    function addUser(string _name, bool _carType) payable public {
 
-function expectedCost()
+        require(msg.value > 0.25 ether);
+        accounts[msg.sender].name = _name;
+        accounts[msg.sender].carLender.carType = _carType;
+        accounts[msg.sender].carLender.available = true;
 
+        //give mobcoins to user
+        MobCoin mobcoin = MobCoin(mobcoinAddress);
+        mobcoin.transfer(msg.sender, msg.value * 1000);
 
+    }
 
 }
